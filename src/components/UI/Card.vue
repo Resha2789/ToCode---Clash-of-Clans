1a7e9f3158d95d6fa5be9d93e4313d7d5a381e6d
r.gabdrashitov <resha_goblin@mail.ru>
2022-09-01 21:40:48 +0500

    slot

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,26 @@
+<!-- @format -->
+
+<template>
+	<div class="card">
+		<span class="card-name">{{ name }}</span>
+		<span class="card-title">{{ title }}</span>
+		<div class="card-body">
+			<slot>Description</slot>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true
+		},
+		title: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,37 @@
 <!-- @format -->
 
 <template>
-	<p>home url</p>
+	<div class="mb-body">
+		<div class="card_wrapper" v-for="(item, id) in items" :key="id">
+			<card :name="`${item.lvl} level`" :title="item.title">
+				{{ item.descr }}
+			</card>
+		</div>
+	</div>
 </template>
+
+<script>
+import Card from '@/components/UI/Card.vue'
+
+export default {
+	components: { Card },
+	data() {
+		return {
+			items: [
+				{
+					id: 1,
+					title: 'Archer',
+					descr: 'Archer, Archer, Archer',
+					lvl: 4
+				},
+				{
+					id: 2,
+					title: 'Wizard',
+					descr: 'Wizard, Wizard, Wizard',
+					lvl: 5
+				}
+			]
+		}
+	}
+}
+</script>
